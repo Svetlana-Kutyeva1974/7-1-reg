923f35a25c67ac009272c974027caf224b4156e3
Svetlana Kutyova <kutyovas@yandex.ru>
2021-11-04 23:36:08 +0300

    2

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 // TODO: write your code here
 import Validator from './basic.js';
-
+// eslint-disable-next-line no-unused-vars
 const obj = new Validator('4578');
+// eslint-disable-next-line no-unused-vars
 const obj1 = new Validator('vf457nh8');
+// eslint-disable-next-line no-unused-vars
 const obj2 = new Validator('_vf4578');
